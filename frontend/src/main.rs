71730234326bd6c39e7f63144b6c845d82fa2e4b
crypto_server <nan.cheng@unisg.ch>
2023-10-27 15:56:27 +0200

    debug

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -150,7 +150,7 @@ async fn main() {
         }
 
         if !is_server{
-            sleep(Duration::from_secs((5+(i/5)*10).try_into().unwrap()));
+            sleep(Duration::from_secs((5+(i/4)*10).try_into().unwrap()));
         }
     }
 }
