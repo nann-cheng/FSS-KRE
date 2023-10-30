163679c60765f31dc661b1942bab37e743e58ecd
Nan Cheng <nan.cheng@unisg.ch>
2023-10-30 15:41:56 +0100

    mods

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -35,11 +35,11 @@ pub enum TEST_OPTIONS{
     TRIVAL_FSS_KRE= 6
 }
 
-pub const M_TEST_CHOICE: TEST_OPTIONS = TEST_OPTIONS::BATCH_KRE;
+pub const M_TEST_CHOICE: TEST_OPTIONS = TEST_OPTIONS::BITWISE_KRE;
 pub const TEST_WAN_NETWORK: bool = true;
 
 //n: input domain length
-const INPUT_BITS: usize = 15usize;
+const INPUT_BITS: usize = 30usize;
 const BATCH_SIZE: usize = 3usize;
 const K_GLOBAL: u32 = 1;
 
