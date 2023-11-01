bdee91ea5fd6e5e5002ef807014ebfe75b7b91c1
Nan Cheng <nan.cheng@unisg.ch>
2023-11-01 13:44:02 +0100

    mods

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -40,7 +40,7 @@ pub const TEST_WAN_NETWORK: bool = true;
 
 //n: input domain length
 const INPUT_BITS: usize = 30usize;
-const BATCH_SIZE: usize = 6usize;
+const BATCH_SIZE: usize = 5usize;
 const K_GLOBAL: u32 = 1;
 
 #[tokio::main]
@@ -68,7 +68,8 @@ async fn main() {
         eprintln!("No arguments provided.");
     }
 
-    let BENCHMARK_PROTOCOL_TYPES:Vec<TEST_OPTIONS> = vec![TEST_OPTIONS::BITWISE_MAX];
+    // let BENCHMARK_PROTOCOL_TYPES:Vec<TEST_OPTIONS> = vec![TEST_OPTIONS::BITWISE_MAX,BATCH_MAX,TRIVAL_FSS_MAX];
+    let BENCHMARK_PROTOCOL_TYPES:Vec<TEST_OPTIONS> = vec![TEST_OPTIONS:BATCH_KRE];
     // let BENCHMARK_PROTOCOL_TYPES:Vec<TEST_OPTIONS> = vec![TEST_OPTIONS::BITWISE_MAX,TEST_OPTIONS::BATCH_MAX,TEST_OPTIONS::BITWISE_KRE,TEST_OPTIONS::BATCH_KRE];
     // let BENCHMARK_PROTOCOL_TYPES:Vec<TEST_OPTIONS> = vec![TEST_OPTIONS::BITWISE_KRE,TEST_OPTIONS::BATCH_KRE, TEST_OPTIONS::TRIVAL_FSS_KRE];
     for protocol in BENCHMARK_PROTOCOL_TYPES{
@@ -77,9 +78,9 @@ async fn main() {
 
             let M_TEST_CHOICE: TEST_OPTIONS = protocol;
 
-        let INPUT_PARAMETERS:Vec<usize> = vec![100,1000,10000,100000,1000000];
+        // let INPUT_PARAMETERS:Vec<usize> = vec![100,1000,10000,100000,1000000];
         // let INPUT_PARAMETERS:Vec<usize> = vec![10,30,50,100];
-        // let INPUT_PARAMETERS:Vec<usize> = vec![500000];
+        let INPUT_PARAMETERS:Vec<usize> = vec![1000];
         for i in 0..INPUT_PARAMETERS.len(){
             let input_size = INPUT_PARAMETERS[i];
             gen_offlinedata(&M_TEST_CHOICE, input_size);
