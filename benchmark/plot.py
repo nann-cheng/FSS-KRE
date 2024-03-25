eb79d1beec569c790826898558fd7f6feaa5949a
crypto_server <nan.cheng@unisg.ch>
2024-03-25 10:05:00 +0100

    update plot codes

--- benchmark/plot.py
+++ benchmark/plot.py
@@ -3,7 +3,8 @@ import numpy as np
 
 
 # Define the range of x values
-x = np.linspace(100, 10000000,num=10)
+x = np.logspace(np.log10(100),np.log10(10000000),num=10)
+
 shape = (10,)
 
 # Define the linear equations
@@ -16,25 +17,28 @@ y1 = (9+2)*np.ceil(np.log2(x))
 # along with one more round for two multiplication
 y2 = (7+1)*np.ceil(np.log2(x))
 y3 = (1+1)*np.ceil(np.log2(x))
-y4 = np.full(shape,2*32-1)
+# y00000 = (3+1)*np.ceil(np.log2(x))
+y4 = np.full(shape,32+1)
 
 
 y11 = (10+2)*np.ceil(np.log2(x))
 y22 = (10+1)*np.ceil(np.log2(x))
-y44 = np.full(shape,2*64-1)
+y44 = np.full(shape,64+1)
 
 
 # Plotting the lines with different line styles and markers
-plt.plot(x, y1, linestyle='-', marker='x', label='COU18, n=32')
-plt.plot(x, y2, linestyle='--', marker='x', label='Damgaard19, n=32')
-plt.plot(x, y3, linestyle=':', marker='x', label='Boyle19, n=32/64')
-plt.plot(x, y4, linestyle='-.', marker='x', label='$\Pi_{Max1}$, n=32')
+plt.plot(x, y1, linestyle='-', marker='o', label='COU18, n=32')
+plt.plot(x, y2, linestyle='--', marker='o', label='Catrina10/Damgård19, n=32')
+plt.plot(x, y3, linestyle=':', marker='o', label='Boyle19, n=32/64')
+# plt.plot(x, y00000, linestyle='-', marker='o', label='Catrina10, n=32/64')
+
+plt.plot(x, y4, linestyle='-.', marker='x', label='Our $\Pi_{Max}$, n=32')
 
 plt.plot(x, y11, linestyle='-', marker='^', label='COU18, n=64')
-plt.plot(x, y22, linestyle='--', marker='^', label='Damgaard19, n=64')
-plt.plot(x, y44, linestyle='-.', marker='^', label='$\Pi_{Max1}$, n=64')
+plt.plot(x, y22, linestyle='--', marker='^', label='Catrina10/Damgård19, n=64')
+plt.plot(x, y44, linestyle='-.', marker='^', label='Our $\Pi_{Max}$, n=64')
 
-plt.xscale('log')
+plt.xscale('log',base=2)
 
 
 # Adding a legend to differentiate the lines
--- benchmark/plot2.py
+++ benchmark/plot2.py
@@ -5,7 +5,8 @@ def max1commuFunc(n):
 	return 3*(n**2) -n
 
 # Define the range of x values
-x = np.linspace(100, 10000000,num=10)
+x = np.logspace(np.log10(100),np.log10(10000000),num=10)
+
 shape = (10,)
 
 # Define the linear equations
@@ -20,25 +21,22 @@ y2 = x*(6*32-8+2*32)/1024**2
 y3 = x*(3*32)/1024**2
 y4 = (x*32+max1commuFunc(32))/1024**2
 
-y11 = x*(1286+1+2*64)/1024**2
-y22 = x*(8*64-8)/1024**2
-y33 = x*(3*64)/1024**2
-y44 = (x*64+max1commuFunc(64))/1024**2
+y33 = x*(3*(32+40))/1024**2
+
 
 
 # Plotting the lines with different line styles and markers
-plt.plot(x, y1, linestyle='-', marker='x', label='COU18, n=32')
-plt.plot(x, y2, linestyle='--', marker='x', label='Damgaard19, n=32')
-plt.plot(x, y3, linestyle=':', marker='x', label='Boyle19, n=32')
-plt.plot(x, y4, linestyle='-.', marker='x', label='$\Pi_{Max1}$, n=32')
+plt.plot(x, y33, linestyle=':', marker='o', label='Catrina10, n=32')
+plt.plot(x, y1, linestyle='-', marker='o', label='COU18, n=32')
+plt.plot(x, y2, linestyle='--', marker='o', label='Damgård19, n=32')
+plt.plot(x, y3, linestyle=':', marker='o', label='Boyle19, n=32')
+plt.plot(x, y4, linestyle='-.', marker='o', label='Our $\Pi_{Max}$, n=32')
 
-# plt.plot(x, y11, linestyle='-', marker='^', label='COU18, n=64')
-# plt.plot(x, y22, linestyle='--', marker='^', label='Damgaard19, n=64')
-# plt.plot(x, y33, linestyle=':', marker='^', label='Boyle19, n=64')
-# plt.plot(x, y44, linestyle='-.', marker='^', label='$\Pi_{Max1}$, n=64')
 
-plt.xscale('log')
+# plt.xscale('log')
 plt.yscale('log')
+
+plt.xscale('log',base=2)
 
 
 # Adding a legend to differentiate the lines
